0be856e2b68112be2bae633045a83e7fe8810db9
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-02-05 23:46:37 +0500

    Loading meals data into MealScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:ai_flutter_application/data/dummy_data.dart';
 import 'package:ai_flutter_application/screens/categories_screen.dart';
+import 'package:ai_flutter_application/screens/meals_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -23,7 +25,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: CategoriesScreen(),
+      home: MealsScreen(
+        title: 'Some categories',
+        meals: dummyMeals,
+      ),
+      // home: CategoriesScreen(),
     );
   }
 }
---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:ai_flutter_application/models/meal.dart';
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    Key? key,
+    required this.title,
+    required this.meals,
+  }) : super(key: key);
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "Uh oh ... nothing here!",
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Try selecting different category!",
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
+  }
+}
