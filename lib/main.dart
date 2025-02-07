29fdfa15877786ff86edad278c75e08ef525d924
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-02-07 20:59:04 +0500

    Creating and using a Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ai_flutter_application/screens/categories_screen.dart';
 import 'package:ai_flutter_application/screens/tabs_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,7 +15,7 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(ProviderScope(child: const App()));
 }
 
 class App extends StatelessWidget {
